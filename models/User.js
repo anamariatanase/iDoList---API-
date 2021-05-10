f304b4bbcd1da26f79004ddf5889f06d28920a0f
Ana <tanase.anamaria97@gmail.com>
2021-05-10 20:15:51 +0300

    Changed User model

--- models/User.js
+++ models/User.js
@@ -2,32 +2,37 @@ const mongoose = require('mongoose');
 
 const UserSchema = mongoose.Schema({
     username: {
-        type:String,
-        required:true,
-        unique:true,
+        type: String,
+        required: true,
+        unique: true,
     },
     password: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     },
-    firstName:{
-        type:String,
-        required:true,
+    firstName: {
+        type: String,
+        required: true,
     },
     lastName: {
-        type:String,
-        required:true,
+        type: String,
+        required: true,
     },
-    email:{
-        type:String,
-        required:true,
-        unique:true,
+    email: {
+        type: String,
+        required: true,
+        unique: true,
     },
     app_background: String,
     lists: [{
-        id: Number,
-        title: String,
-        description: String,
+        list: {
+            listName: String,
+            title: String,
+            cards: [{
+                content: String
+            }]
+        }
+
     }]
 })
 module.exports = mongoose.model('Users', UserSchema);
